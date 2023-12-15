46652ae0b33e870de751642315c2d97d730fbc41
xavierace990 <141823546+xavierace990@users.noreply.github.com>
2023-12-15 12:49:01 -0800

    settings.py

--- form/form/settings.py
+++ form/form/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1g1a#021$%2o&$=g%htb*5hwr4lx6plq68o9@itqz%t^e$v!j0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','churchofchristojodu.org']
+ALLOWED_HOSTS = []
 
 
 # Application definition
