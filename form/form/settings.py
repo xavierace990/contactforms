cb9a4e3937f53a5e36c1bf672f7eb5fb28fb1874
xavierace990 <141823546+xavierace990@users.noreply.github.com>
2023-12-15 12:52:37 -0800

    settings.py

--- form/form/settings.py
+++ form/form/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1g1a#021$%2o&$=g%htb*5hwr4lx6plq68o9@itqz%t^e$v!j0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
